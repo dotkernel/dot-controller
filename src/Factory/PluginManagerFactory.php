fc843e3b85ba43743807c0f6f5fd1d98c654ede1
n3vrax <n3vrax@gmail.com>
2016-09-05 21:18:03 +0300

    registered the 2 built in plugins into the manager

--- src/Factory/PluginManagerFactory.php
+++ src/Factory/PluginManagerFactory.php
@@ -10,7 +10,11 @@
 namespace DotKernel\DotController\Factory;
 
 use DotKernel\DotController\Plugin\PluginManager;
+use DotKernel\DotController\Plugin\TemplatePlugin;
+use DotKernel\DotController\Plugin\UrlHelperPlugin;
 use Interop\Container\ContainerInterface;
+use Zend\Expressive\Helper\UrlHelper;
+use Zend\Expressive\Template\TemplateRendererInterface;
 
 /**
  * Class PluginManagerFactory
@@ -25,6 +29,20 @@ class PluginManagerFactory
     public function __invoke(ContainerInterface $container)
     {
         $pluginManager = new PluginManager($container, $container->get('config')['dk_controller']['plugin_manager']);
+
+        //register the built in plugins, if the required component is present
+        if ($container->has(UrlHelper::class)) {
+            $pluginManager->setFactory('url', function (ContainerInterface $container) {
+                return new UrlHelperPlugin($container->get(UrlHelper::class));
+            });
+        }
+
+        if ($container->has(TemplateRendererInterface::class)) {
+            $pluginManager->setFactory('template', function (ContainerInterface $container) {
+                return new TemplatePlugin($container->get(TemplateRendererInterface::class));
+            });
+        }
+
         return $pluginManager;
     }
 }
